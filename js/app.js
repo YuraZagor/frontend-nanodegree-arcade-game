be79eae427b8d1deb105e2ad8998b670a47de2c4
YuraZagor <u.zagorulko@gmail.com>
2022-09-12 17:05:15 +0300

    Frogger on classes, collision does not work

--- js/app.js
+++ js/app.js
@@ -1,46 +1,92 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Animate {
+    constructor(x, y, sprite){
+        this.x = x;
+        this.y = y;
+        this.sprite=sprite;
+    };
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };    
+}
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+let allEnemies = []
+let allSpeeds = [100, 200, 300]
+let rangeY = [73, 156, 239]
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+class Enemy extends Animate {
+    constructor(x, y, sprite, speedX) {
+        super(x, y, sprite);
+        this.speedX = speedX;
+    }
+    update = function(dt) {
+        this.x += this.speedX * dt;           
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
+Enemy.prototype.checkCollision = function() {
+    if ((this.x < player.x)  &&  (player.x - this.x < 30 )){
+        if (player.y === this.y){
+            console.log("Ooooops!")
+        }
+    }   
+}
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+function addBug() {
+    rangeY.forEach (
+        (levelMark)=>  allEnemies.push( new Enemy(0, levelMark, 'images/enemy-bug.png', allSpeeds[Math.floor(Math.random()*3)] )) 
+    )       
+}
+addBug()
+setInterval(() => addBug(), 3000)
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
+class Player extends Animate {
+    constructor(x, y, sprite) {
+        super(x, y, sprite);
+    }
+    backToStart(){
+        this.x = 202;
+        this.y = 405;
+    }
+    update = function() {
+        if (this.y < 0){
+           this.backToStart()
+        }
+    };
+}
+
+let player = new Player(202, 405, 'images/char-boy.png')
 
 
+Player.prototype.handleInput = function (direction) {
+    switch (direction) {
+        case 'left':
+            if (this.x > 0){
+                this.x -= 101 };
+                break;
+        case 'up':
+            this.y -= 83;
+            break;
+        case 'right':
+            if (this.x < 400){
+                this.x += 101};
+                break;
+        case 'down':
+            if (this.y < 400){
+                this.y += 83 };
+            break;
+        default:
+            break;
+    };        
+} 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
-});
+})
