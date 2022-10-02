3983c309d30aec8e813a55e049b68599af584418
YuraZagor <u.zagorulko@gmail.com>
2022-10-02 18:09:50 +0300

    added player passed as an attr

--- js/app.js
+++ js/app.js
@@ -16,36 +16,36 @@ const row1 = cellY + spriteRepositioningY
 const row2 = 2*cellY + spriteRepositioningY
 const row3 = 3*cellY + spriteRepositioningY
 const rangesY = [row1, row2, row3];
-function Player(x, y, sprite) {
+
+function Character (x, y, sprite) {
     this.x = x;
     this.y = y;
     this.sprite=sprite;
 };
-Player.prototype.render = function() {
+
+Character.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y) 
 };
-Player.prototype.update = function() {
-};
-const player = new Player ( playerStartX, playerStartY, 'images/char-boy.png' );
+
 function Enemy(x, y, sprite, speedX) {
-    Player.call(this, x, y, sprite);
+    Character.call(this, x, y, sprite);
     this.speedX = speedX;
 };
-Enemy.prototype = Object.create(Player.prototype);
+Enemy.prototype = Object.create(Character.prototype);
+
 Enemy.prototype.update = function(dt){
     this.x += this.speedX * dt;
     if (this.x > fieldWidth){
         this.x = enemyRestartX;
         this.speedX = allSpeeds[Math.floor(Math.random()*3)];
     };
-    this.checkColision()
+    this.checkColision(player)
 }
-Enemy.prototype.checkColision = function(){
-
-    if (this.y === player.y){
-        if ((this.x - player.x < collisionOffset ) && (player.x - this.x < collisionOffset )) {
-            player.x = playerStartX;
-            player.y = playerStartY;
+Enemy.prototype.checkColision = function(checked){    
+    if (this.y === checked.y){
+        if ((this.x - checked.x < collisionOffset ) && (checked.x - this.x < collisionOffset )) {
+            checked.x = playerStartX;
+            checked.y = playerStartY;
         };    
     };
 }
@@ -55,6 +55,16 @@ function addBug() {
     .push( new Enemy(0, levelMark, 'images/enemy-bug.png', allSpeeds[Math.floor(Math.random()*3)] )) 
     )
 };
+
+function Player(x, y, sprite) {
+    Character.call(this, x, y, sprite);
+};
+
+Player.prototype = Object.create(Character.prototype);
+
+Player.prototype.update = function(dt){
+}
+
 Player.prototype.handleInput = function (direction) {
     switch (direction) {
         case 'left':
@@ -93,6 +103,6 @@ document.addEventListener('keyup', function(e) {
     };
     player.handleInput(allowedKeys[e.keyCode]);
 });
-    
+const player = new Player ( playerStartX, playerStartY, 'images/char-boy.png' ); 
 addBug()
 
